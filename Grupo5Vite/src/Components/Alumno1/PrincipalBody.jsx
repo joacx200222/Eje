5f7c1a10eeddee73309944a65b200b1b3856fbb3
RicardoHuapa <106850259+RicardoHuapa@users.noreply.github.com>
2024-05-13 18:54:35 -0500

    Avance 2 - Página principal

--- Grupo5Vite/src/Components/Alumno1/PrincipalBody.jsx
+++ Grupo5Vite/src/Components/Alumno1/PrincipalBody.jsx
@@ -8,7 +8,7 @@ function PrincipalBody(){
             <ul>
                 <li><img src="" className='ImagenItem1' alt="imagenitem1" /></li>
                 <li><p>Colección de items 1: Especiales para regresar al colegio</p></li>
-                <li><h6><p>learn more</p></h6></li>
+                <li><h6><p><link to ="/"/>learn more</p></h6></li>
 
             </ul>
             <ul>
@@ -24,6 +24,64 @@ function PrincipalBody(){
 
             </ul>
         </ul>
+        <br/><br/>
+        <ul className='PrincipalBody1'>
+            <ul>
+                <li><img src="" className='ImagenItem4' alt="imagenitem4"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem5' alt="imagenitem5"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem6' alt="imagenitem6"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem7' alt="imagenitem7"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem8' alt="imagenitem8"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem9' alt="imagenitem9"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem10' alt="imagenitem10"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem10' alt="imagenitem10"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem11' alt="imagenitem11"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem12' alt="imagenitem12"/></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+            </ul>
+        </ul>
+        <hr/>
+
+        <h3>Nuevos</h3>
+            
+        
         </>
 
     )
