62f289a6cf413fefcb2b66ca9893afd5fd050b40
SergioAvilat <20202547@aloe.ulima.edu.pe>
2024-05-12 23:06:16 -0500

    Cambios Footer V1

--- Grupo5Vite/src/Componentes/Pie.jsx
+++ Grupo5Vite/src/Componentes/Pie.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../App.css'
+import Facebook from '../assets/imagenes/facebook.png'
+import Instagram from '../assets/imagenes/instagram.png'
+import Twitter from '../assets/imagenes/twitter.png'
+import Youtube from '../assets/imagenes/youtube.png'
 
 
 function Pie(){
@@ -8,37 +12,38 @@ function Pie(){
         <>
         <footer>
             <ul className='primerfooter'>
-                <li id="LaTienditaDelAbuelo">LA TIENDITA DEL ABUELO</li>
+                <li id="LaTienditaDelAbuelo"><strong>LA TIENDITA DEL ABUELO</strong></li>
                 <li>© 2010 - 2020</li>
                 <li>Privacy - Terms</li>
 
             </ul>
             <ul className='segundofooter'>
-                <li id="Segundotitle">Cuenta</li>
+                <li id="Segundotitle"><strong>Cuenta</strong></li>
                 <li>Login</li>
                 <li>Registro</li>
                 <li>Carrito</li>
 
             </ul>
             <ul className='tercerofooter'>
-                <li id="tercerotitle">Productos</li>
+                <li id="tercerotitle"><strong>Productos</strong></li>
                 <li>Mas vendidos</li>
                 <li>Nuevos</li>
                 <li>Ofertas</li>
 
             </ul>
             <ul className='cuartofooter'>
-                <li id="cuartotitle">Ayuda</li>
+                <li id="cuartotitle"><strong>Ayuda</strong></li>
                 <li> Acerca de Nosotros</li>
                 <li> Politica de Envio</li>
                 <li> FAQ </li>
 
             </ul>
             <ul className='quintofooter'>
-                <li><img src="../assets/imagenes/facebook.png"></img></li> 
-                <li><img src="../assets/imagenes/instagram.png"></img></li> 
-                <li><img src="../assets/imagenes/twitter.png"></img></li> 
-                <li><img src="../assets/imagenes/youtube.png"></img></li> 
+                <li><img src={Facebook} className="CarritoCompras" alt="facebook"/></li> 
+                <li><img src={Instagram} className="CarritoCompras" alt="instagram"/></li> 
+                <li><img src={Twitter} className="CarritoCompras" alt="twitter"/></li> 
+                <li><img src={Youtube} className="CarritoCompras" alt="youtube"/></li> 
+
             </ul>
         </footer>
         </>
