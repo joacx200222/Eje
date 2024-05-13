dcfea2eb71e2d6f94b02fc78ad5b2498b82a378a
SergioAvilat <20202547@aloe.ulima.edu.pe>
2024-05-12 20:59:12 -0500

    Cambios Header y Css Header

--- Grupo5Vite/src/Componentes/Cabecera1.jsx
+++ Grupo5Vite/src/Componentes/Cabecera1.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import Carrito from '../assets/imagenes/carrito.png'
 import { Link } from "react-router-dom";
+import '../App.css'
 
 function Cabecera1(){
     return (
         <>
         <header>
             <ul id="izquierda">
-            <li id = "Título"><Link to = "/">Tienda</Link></li>
-            <li>Más vendidos</li>
-            <li>Nuevos</li>
-            <li>Ofertas</li>
+                <li id="Titulo" ><strong><Link to = "/">Tienda</Link></strong></li>
+                <li>Más vendidos</li>
+                <li>Nuevos</li>
+                <li>Ofertas</li>
             </ul>
             <ul id="derecha">
-            <li><Link to = "/Carrito">Tienda</Link><img src={Carrito} className="CarritoCompras" alt="CarritodeCompras"/></li>
-            <li>Ayuda</li>
-            <li><Link to = "/Cuenta">Tienda</Link><button id="Micuenta">Mi Cuenta</button></li>
+                <li><Link to = "/Carrito"><img src={Carrito} className="CarritoCompras" alt="CarritodeCompras"/></Link></li>
+                <li>Ayuda</li>
+                <li><Link to = "/Cuenta"><button id="Micuenta">Mi Cuenta</button></Link></li>
             </ul> 
         </header>
+        
         </>
     )
 }
