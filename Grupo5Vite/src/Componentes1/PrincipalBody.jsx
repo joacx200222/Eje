381ad6962a2f029d98483c2055c22e0f2be4d0ba
RicardoHuapa <106850259+RicardoHuapa@users.noreply.github.com>
2024-05-14 12:15:13 -0500

    Página Principal Base

--- Grupo5Vite/src/Componentes1/PrincipalBody.jsx
+++ Grupo5Vite/src/Componentes1/PrincipalBody.jsx
@@ -80,7 +80,58 @@ function PrincipalBody(){
         <hr/>
 
         <h3>Nuevos</h3>
-            
+        <ul className='PrincipalBody2'>
+            <ul>
+                <li><img src="" className='ImagenItem13' alt="imagenitem13" /></li>
+                <li><p>Colección de items 1: Especiales para regresar al colegio</p></li>
+                <li><h6><p><link to ="/"/>learn more</p></h6></li>
+
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem14' alt="imagenitem14" /></li>
+                <li><p>Colección de items 2: Especiales para la casa</p></li>
+                <li><h6><p>learn more</p></h6></li>
+
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem15' alt="imagenitem15" /></li>
+                <li><p>Colección de items 3: Especiales para los pequeños</p></li>
+                <li><h6><p>learn more</p></h6></li>
+
+            </ul>
+        </ul>    
+        <ul className='PrincipalBody3'>
+            <ul>
+                <li><img src="" className='ImagenItem16' alt="imagenitem16" /></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p><link to ="/"/>learn more</p></h6></li>
+
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem17' alt="imagenitem17" /></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem18' alt="imagenitem18" /></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem19' alt="imagenitem19" /></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+
+            </ul>
+            <ul>
+                <li><img src="" className='ImagenItem20' alt="imagenitem20" /></li>
+                <li><p>Item 1</p></li>
+                <li><h6><p>learn more</p></h6></li>
+
+            </ul>
+        </ul>    
         
         </>
 
