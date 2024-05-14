0aebd1c64b9bb76db5d3d32bf568a07218c451e1
SergioAvilat <20202547@aloe.ulima.edu.pe>
2024-05-14 17:34:01 -0500

    Cambios importantes De Alumno2 y 1

--- Grupo5Vite/src/Componentes2/CarritoCompras.jsx
+++ Grupo5Vite/src/Componentes2/CarritoCompras.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Carrito.css'
+import Cabecera1 from '../ComponentesGeneral/Cabecera1'
 
 
 
 function CarritoCompras(){
-    <h3>XD</h3>
+    return(
+        <div>
+            <Cabecera1/>
+
+        </div>
+    )
+        
+    
+    
 }
 export default CarritoCompras;
