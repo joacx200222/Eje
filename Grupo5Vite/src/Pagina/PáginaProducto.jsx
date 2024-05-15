0f1650426ffc5b8332f637d3b0f7e0e86a3786ab
Ricardo <106850259+RicardoHuapa@users.noreply.github.com>
2024-05-15 13:19:09 -0500

    base de datos articulos

---
+++ Grupo5Vite/src/Pagina/PáginaProducto.jsx
@@ -0,0 +1,18 @@
+import '../App.css'
+import Cabecera1 from '../ComponentesGeneral/Cabecera1'
+import Pie from '../ComponentesGeneral/Pie'
+
+function PaginaProducto(){
+    return(
+        <>
+        <div>
+            <Cabecera1/>
+
+            <br></br>
+            <Pie/>
+        </div>
+        </>
+    )
+}
+
+export default PaginaProducto
