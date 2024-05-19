7b1bead90aef43831039f6520cbad44f5ecbc511
Ricardo <106850259+RicardoHuapa@users.noreply.github.com>
2024-05-19 07:50:53 -0500

    cambios v1

--- Grupo5Vite/src/Componentes1/Buscador.jsx
+++ Grupo5Vite/src/Componentes1/Buscador.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
+import data from "../Data/articulos.json"
 
 
 function Buscador(){
 
+    const [buscar, setBuscar] = useState("")
+
+
+    useEffect(() => {
+        fetch(data)
+        .then(res => res.json())
+        .then(
+            (resultado) => 
+        )
+    })
+
     return(
     <>
         <div className='buscador'>
-            <input type="text" id="barrabuscador" nombre="barrabuscador" placeholder='Busca productos por nombre...'></input>
+            <input type="text" id="barrabuscador" nombre="barrabuscador" placeholder='Busca productos por nombre...' 
+            value={buscar} onChange={(evento) => setBuscar(evento.target.value)}></input>
             <br></br>
-            <input type="button" value="Buscar" id='buscarProductos'/>
-            
+            <button type='button'>BUSCAR</button>
         </div>
         
     </>
 )}
 
 export default Buscador
-
-/*<button type='button'>BUSCAR</button>*/ 
