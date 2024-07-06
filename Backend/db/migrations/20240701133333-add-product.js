2eca1de349c108daf348b3f243aa20a5be7869fc
Ricardo <106850259+RicardoHuapa@users.noreply.github.com>
2024-07-06 16:08:05 -0500

    DBcompletada

--- Backend/db/migrations/20240701133333-add-product.js
+++ Backend/db/migrations/20240701133333-add-product.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.STRING,
         references: {
           model: "products", // name of Target model
-          key: "id", // key in Target model that we're referencing
+          key: "nombre", // key in Target model that we're referencing
         },
         onUpdate: "CASCADE",
         onDelete: "SET NULL",
