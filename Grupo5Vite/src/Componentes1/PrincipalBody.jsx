626acb830a3fbafc27b2598b9e44b9929c8d8fe0
SergioAvilat <20202547@aloe.ulima.edu.pe>
2024-07-06 21:08:20 -0500

    Cambios

--- Grupo5Vite/src/Componentes1/PrincipalBody.jsx
+++ Grupo5Vite/src/Componentes1/PrincipalBody.jsx
@@ -57,87 +57,10 @@ function PrincipalBody(){
                 <li><p>Item 1</p></li>
                 <li><h6><p>learn more</p></h6></li>
             </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem9' alt="imagenitem9"/></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem10' alt="imagenitem10"/></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem10' alt="imagenitem10"/></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem11' alt="imagenitem11"/></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem12' alt="imagenitem12"/></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-            </ul>
         </ul>
-        <hr/>
-
-        <h3>Nuevos</h3>
-        <ul className='PrincipalBody2'>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem13' alt="imagenitem13" /></li>
-                <li><p>Colección de items 1: Especiales para regresar al colegio</p></li>
-                <li><h6><p><link to ="/"/>learn more</p></h6></li>
-
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem14' alt="imagenitem14" /></li>
-                <li><p>Colección de items 2: Especiales para la casa</p></li>
-                <li><h6><p>learn more</p></h6></li>
-
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem15' alt="imagenitem15" /></li>
-                <li><p>Colección de items 3: Especiales para los pequeños</p></li>
-                <li><h6><p>learn more</p></h6></li>
-
-            </ul>
-        </ul>    
-        <ul className='PrincipalBody3'>
-            <ul>
-                <li><img src={Garfield}className='ImagenItem16' alt="imagenitem16" /></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p><link to ="/"/>learn more</p></h6></li>
-
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem17' alt="imagenitem17" /></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem18' alt="imagenitem18" /></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem19' alt="imagenitem19" /></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-
-            </ul>
-            <ul>
-                <li><img src={Garfield} className='ImagenItem20' alt="imagenitem20" /></li>
-                <li><p>Item 1</p></li>
-                <li><h6><p>learn more</p></h6></li>
-
-            </ul>
-        </ul>    
+        
+        
+      
         
         </>
 
